f4997b7be418f746e65b892bc5d8d0eaa83a8b38
unknown <mohseen_abasi@yahoo.com>
2026-07-20 15:28:15 +0330

    feat(ui) called feature-box

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import NavBar from "./components/header/navbar";
 import ControlSpacing from "./components/layout/control-spacing";
 import Hero from "./components/hero/hero";
+import FeatureBox from "./components/feature-box/feature-box";
 
 export default function Home() {
   return (
     <ControlSpacing>
-      <main className="grid border gap-section-mobile tab:gap-section-tablet">
+      <main className="grid  gap-section-mobile tab:gap-section-tablet">
         <NavBar />
         <Hero />
+        <FeatureBox/>
       </main>
     </ControlSpacing>
   );
