e9583bd91202bc9e20586c13067b9ff2c1a1d322
unknown <mohseen_abasi@yahoo.com>
2026-07-22 12:02:23 +0330

    feat(render) watch-slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-import Image from "next/image";
 import NavBar from "./components/header/navbar";
 import ControlSpacing from "./components/layout/control-spacing";
 import Hero from "./components/hero/hero";
 import FeatureBox from "./components/feature-box/feature-box";
+import Titel from "./components/titel/titel";
+import WatchSlider from "./components/watch-slider/slider";
+import ComeOne from "./components/slider-test/ComOne";
+import ComeTwo from "./components/slider-test/ComeTwo";
+import ComeThree from "./components/slider-test/ComeThree";
 
 export default function Home() {
   return (
     <ControlSpacing>
-      <main className="grid  gap-section-mobile tab:gap-section-tablet">
+      <main className="grid  gap-section-mobile tab:gap-section-tablet xl:gap-section-xl">
         <NavBar />
         <Hero />
-        <FeatureBox/>
+        <FeatureBox />
+        <Titel />
+        <WatchSlider />
+ 
       </main>
     </ControlSpacing>
   );
