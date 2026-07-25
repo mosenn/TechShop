bce264813e16ec381c24fdf9d90ede11713f41a0
unknown <mohseen_abasi@yahoo.com>
2026-07-25 12:35:11 +0330

    fix(finish) Slider Is Done

--- app/components/watch-slider/slider.tsx
+++ app/components/watch-slider/slider.tsx
@@ -1,116 +1,187 @@
+"use client";
+
 import { ArrowLeft, BatteryFull, Drop, HeartAdd } from "iconsax-reactjs";
 import Image from "next/image";
 import React from "react";
 import styles from "./GlassCircle.module.css";
 
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Scrollbar, A11y } from "swiper/modules";
+
+const watchSlides = [
+  {
+    id: 1,
+    image: "/imgs/watchs/1.png",
+    imageAlt: "watch",
+    title: "ساعت هوشمند",
+    name: "Galexy Watch8",
+    size: "48mm",
+    description:
+      "ترکیبی بی نظیر از طراحی کلاسیک و تکنولوژی هوشمند . مناسب برای ورزش و کار و زندگی روزمره با امکانات پیشرفته سلامت و اتصال هوشمند",
+    battery: "3 روز",
+  },
+  {
+    id: 2,
+    image: "/imgs/watchs/tactix_8_51mm_Solar.png",
+    imageAlt: "tactix_8_51mm_Solar",
+    title: "ساعت هوشمند",
+    name: "Garmin - Tactix SOLAR",
+    size: "51mm",
+    description:
+      "گارمین دستیار نظامی قدرتمند، فوق‌سخت و تاکتیکال. ایده‌آل برای تمرینات سنگین و با دوام بی‌نظیر و تکنولوژی پیشرفته.",
+    battery: "30 روز",
+  },
+  {
+    id: 3,
+    image: "/imgs/watchs/61MHW8rFw6L.png",
+    imageAlt: "Amazfit Active 3",
+    title: "ساعت هوشمند",
+    name: "Amazfit Active 3",
+    size: "45mm",
+    description:
+      "همراه حرفه‌ای ورزشکاران؛ با نمایشگر کریستال یاقوت (Sapphire) و GPS دقیق. این ساعت با پشتیبانی از نقشه‌های آفلاین، ۱۷۰+ مود ورزشی",
+    battery: "12 روز",
+  },
+];
+
+const featureItems = [
+  {
+    id: 1,
+    icon: HeartAdd,
+    text: "پایش هوشمند سلامت",
+  },
+  {
+    id: 2,
+    icon: Drop,
+    text: "مقاوم در برابر اب   5 ATM",
+  },
+  {
+    id: 3,
+    icon: BatteryFull,
+    text: "شارژ باتری",
+    rotate: true,
+  },
+];
+
 const WatchSlider = () => {
   return (
-    <main className="relative border grid gap-5">
-      {/* watch image */}
+    <main className="relative    grid gap-5">
+      <div className="relative    overflow-hidden">
+        <Swiper
+          className="  "
+          modules={[Pagination, Scrollbar, A11y]}
+          spaceBetween={0}
+          slidesPerView={1}
+          navigation
+          pagination={{
+            clickable: true,
+            el: `.${styles.heroPagination}`,
+            bulletClass: styles.paginationBullet,
+            bulletActiveClass: styles.paginationBulletActive,
+          }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {watchSlides.map((item) => (
+            <SwiperSlide key={item.id} className=" w-fit  ">
+              <main className="grid  gap-5 tab:grid-cols-2 lg:p-4 xl:p-8">
+                <section className="relative isolate overflow-hidden">
+                  {/* Glow */}
+                  <div
+                    className="pointer-events-none absolute left-1/2 top-1/2 z-0
+                 h-102 w-96 -translate-x-1/2 -translate-y-1/2
+                 rounded-full opacity-50 blur-[140px]
+                 bg-[radial-gradient(circle_at_center,rgba(14,165,233,0.4)_0%,rgba(14,165,233,0)_100%)]"
+                  />
 
-      <section className="relative isolate overflow-hidden">
-        {/* Glow */}
-        <div
-          className="pointer-events-none absolute left-1/2 top-1/2 z-0
-               h-[407px] w-[385px] -translate-x-1/2 -translate-y-1/2
-               rounded-full opacity-50 blur-[140px]
-               bg-[radial-gradient(circle_at_center,rgba(14,165,233,0.4)_0%,rgba(14,165,233,0)_100%)]"
-        />
+                  {/* دایره و ساعت */}
+                  <figure className="relative z-20 flex items-center justify-center ">
+                    <div className={`${styles.glassCircle}`}>
+                      <Image
+                        src={item.image}
+                        className="mt-12 w-[80%] h-auto xl:w-[90%]"
+                        alt={item.imageAlt}
+                        width={300}
+                        height={300}
+                      />
+                    </div>
+                  </figure>
+                </section>
 
-        {/* Glass بزرگ جدید */}
-        {/* <div
-          className={`${styles.largeGlass} pointer-events-none absolute rotate-10 rounded-full
-                left-1/2 top-1/2 z-10
-                h-[380px] w-[390px]
-                -translate-x-1/2 -translate-y-1/2 `}
-        /> */}
+                {/* texts & buttons (info) */}
+                <section className=" gap-2 flex flex-col xl:items-center">
+                  {/* texts */}
+                  <div className="   text-right  xl:text-center *:my-2">
+                    <h1 className="font-normal text-xl xl:text-[40px]">
+                      {item.title}
+                    </h1>
+                    <h2 className="font-medium text-[1rem] mx-6 xl:text-[36px]">
+                      {item.name}
+                    </h2>
+                    <p className="font-bold text-[1rem] gap-2 flex w-[96%] justify-end xl:justify-center">
+                      Classsic
+                      <span className="border border-[#16A2AB]   p-2 flex items-center justify-center text-center w-[54px] h-[22px] mx-2  text-[#16A2AB] rounded-[32px] font-medium text-[12px]">
+                        {item.size}
+                      </span>
+                    </p>
+                    <article className="flex justify-end">
+                      <p className="font-normal text-[1rem] text-brand-text-secondary w-[327px]    ">
+                        {item.description}
+                      </p>
+                    </article>
+                  </div>
 
-        {/* دایره و ساعت */}
-        <figure className="relative z-20 flex items-center justify-center ">
-          <div className={styles.glassCircle}>
-            <Image
-              src="/imgs/watchs/1.png"
-              className="mt-12"
-              alt="watch"
-              width={300}
-              height={300}
-            />
-          </div>
-        </figure>
-      </section>
+                  {/* icons-texts */}
+                  <ul className="   flex justify-end *:[li]:w-[75px] ">
+                    {featureItems.map((feature) => {
+                      const Icon = feature.icon;
 
-      {/*  */}
-      <section className="flex flex-col ">
-        {/* texts */}
-        <div className="border text-right *:my-2">
-          <h1 className="font-normal text-xl">ساعت هوشمند</h1>
-          <h2 className="font-bold text-[1rem] mx-6">Galexy Watch8</h2>
-          <p className="font-bold text-[1rem] mx-4 flex w-[96%]   justify-end ">
-            Classsic{" "}
-            <span className="border p-2 flex items-center justify-center text-center w-[54px] h-[22px] mx-2 border-[#16A2AB] text-[#16A2AB] rounded-[32px] font-medium text-[12px]">
-              48mm
-            </span>
-          </p>
-          <article>
-            <p className="font-normal text-[1rem] text-brand-text-secondary">
-              ترکیبی بی نظیر از طراحی کلاسیک و تکنولوژی هوشمند . مناسب برای ورزش
-              و کار و زندگی روزمره با امکانات پیشرفته سلامت و اتصال هوشمند
-            </p>
-          </article>
-        </div>
-        {/* icons-texts */}
-        <ul className="border flex  justify-end  *:[li]:w-[75px] *:[li]:border *:[li]:border-amber-200 ">
-          <li className=" flex flex-col items-center text-center text-[12px] font-light text-[#F3F3F3]">
-            <HeartAdd size="32" color="#FF8A65" className="my-2" />
-            پایش هوشمند سلامت
-          </li>
-          <li className=" flex flex-col items-center text-center text-[12px] font-light text-[#F3F3F3]">
-            <Drop size="32" color="#FF8A65" className="my-2" />
-            مقاوم در برابر اب 5ATM
-          </li>
-          <li className=" flex flex-col items-center text-center text-[12px] font-light text-[#F3F3F3]">
-            <BatteryFull
-              size="32"
-              color="#FF8A65"
-              className="rotate-270 my-2"
-            />
-            باتری قدرتمند تا 3 روز
-          </li>
-        </ul>
-        {/* buttons */}
-        <div className="flex  py-4   justify-end">
-          <button className="bg-brand-primary w-36.5 h-11.5 rounded-lg px-2 mx-3 flex justify-center items-center font-normal text-[14px]">
-            <span className="mx-2">
-              <ArrowLeft size="20" color="#FFFFFF" />
-            </span>
-            مشاهده و خرید
-          </button>
-          <button className=" border border-brand-border w-36.5 h-11.5 rounded-lg px-2  flex justify-center items-center font-normal text-[14px]">
-            جزئیات بیشتر
-          </button>
-        </div>
-        {/* dots */}
+                      return (
+                        <li
+                          key={feature.id}
+                          style={{
+                            direction: "rtl",
+                          }}
+                          className=" flex flex-col mx-4 items-center text-center text-[12px] font-light text-[#F3F3F3]"
+                        >
+                          <Icon
+                            size="32"
+                            color="#FF8A65"
+                            className={`my-2 ${feature.rotate ? "rotate-270" : ""}`}
+                          />
 
-        <div className="flex flex-col items-center gap-3 absolute top-[40%] left-[10%] ">
-          {/* Active */}
-          <div className="h-8 w-1 rounded-full bg-cyan-400" />
+                          {feature.text}
 
-          {/* Dot */}
-          <div className="h-1.5 w-1.5 rounded-full bg-zinc-500" />
+                          {/* <span>{item.ATM}</span> */}
 
-          {/* Dot */}
-          <div className="h-1.5 w-1.5 rounded-full bg-zinc-500" />
+                          {feature.id === 3 && <span>{item.battery}</span>}
+                        </li>
+                      );
+                    })}
+                  </ul>
 
-          {/* Active Item */}
-          <div className="h-8 w-1 rounded-full bg-zinc-400" />
+                  {/* buttons */}
+                  <div className="flex py-4 justify-end">
+                    <button className="bg-brand-primary w-36.5 h-11.5 rounded-lg px-2 mx-3 flex justify-center items-center font-normal text-[14px]">
+                      <span className="mx-2">
+                        <ArrowLeft size="20" color="#FFFFFF" />
+                      </span>
+                      مشاهده و خرید
+                    </button>
+                    <button className="     -brand-   w-36.5 h-11.5 rounded-lg px-2 flex justify-center items-center font-normal text-[14px]">
+                      جزئیات بیشتر
+                    </button>
+                  </div>
+                </section>
+              </main>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
-          {/* Dot */}
-          <div className="h-1.5 w-1.5 rounded-full bg-zinc-500" />
-
-          {/* Dot */}
-          <div className="h-1.5 w-1.5 rounded-full bg-zinc-500" />
-        </div>
-      </section>
+      <div className={styles.heroPagination} />
     </main>
   );
 };
