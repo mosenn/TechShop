ae568fc7768a16997d4a1398b171c8a5cb7a026e
unknown <mohseen_abasi@yahoo.com>
2026-07-25 12:39:12 +0330

    start(category) init category section

---
+++ app/components/product-category/category.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Category = () => {
+  return <main>
+    <h1></h1>
+
+
+  </main>;
+};
+
+export default Category;
--- app/components/titel/titel.tsx
+++ app/components/titel/titel.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Titel = () => {
   return (
-    <section className="border flex gap-2 items-center flex-col justify-center">
-      <div className="gap-2 border w-[90%] text-center">
+    <section className=" flex gap-2 items-center flex-col justify-center">
+      <div className="gap-2  w-[90%] text-center">
         <h1 className="text-2xl font-bold my-2 tab:my-4">اصیل ترین ها را از ما بخواهید</h1>
         <h2 className="text-xl font-normal">
           مقایسه کنید بهترین تجربه خرید با ما همیشه همراه شما هستیم
