6ce8de475005f75f6b1bb187b5f31920067705a9
unknown <mohseen_abasi@yahoo.com>
2026-07-27 09:37:51 +0330

    fix(component) fixed structure title component

--- app/components/titel/titel.tsx
+++ app/components/titel/titel.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 
-const Titel = () => {
+type props = {
+  titel: string;
+  underTitle: string;
+  line: true;
+};
+const Titel = ({ titel, underTitle, line }: props) => {
   return (
     <section className=" flex gap-2 items-center flex-col justify-center">
-      <div className="gap-2  w-[90%] text-center">
-        <h1 className="text-2xl font-bold my-2 tab:my-4">اصیل ترین ها را از ما بخواهید</h1>
+      <div className="gap-2  text-center">
+        <h1 className="text-2xl font-bold my-2 tab:my-4">
+          {/* اصیل ترین ها را از ما بخواهید */}
+          {titel}
+        </h1>
         <h2 className="text-xl font-normal">
-          مقایسه کنید بهترین تجربه خرید با ما همیشه همراه شما هستیم
+          {/* مقایسه کنید بهترین تجربه خرید با ما همیشه همراه شما هستیم */}
+          {underTitle}
         </h2>
       </div>
       {/* line */}
-      <div className="bg-brand-accent w-[59px] h-[5px] my-7 border border-brand-accent rounded-sm"></div>
+      {line && (
+        <div className="bg-brand-accent w-[59px] h-[5px] my-7 border border-brand-accent rounded-sm"></div>
+      )}
     </section>
   );
 };
